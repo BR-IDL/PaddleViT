8652e5e8be0bd3bdc138fa0a79f7e9be54b17f38
xperzy <xperzy@gmail.com>
2021-08-10 21:04:46 +0800

    add t2t vit

---
+++ image_classification/T2T_ViT/run_eval_multi_7.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python main_multi_gpu.py \
+-cfg='./configs/t2t_vit_7.yaml' \
+-dataset='imagenet2012' \
+-batch_size=128 \
+-data_path='/dataset/imagenet' \
+-eval \
+-pretrained='./t2t_vit_7' \
