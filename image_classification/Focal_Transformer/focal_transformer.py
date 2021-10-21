047a7deff271e955c62ce874b20e23b37b740207
cjh3020889729 <55022974+cjh3020889729@users.noreply.github.com>
2021-10-21 20:54:27 +0800

    Update focal_transformer.py

--- image_classification/Focal_Transformer/focal_transformer.py
+++ image_classification/Focal_Transformer/focal_transformer.py
@@ -869,7 +869,7 @@ class PatchEmbed(nn.Layer):
 
     def forward(self, x):
         B, C, H, W = x.shape
-        # FIXME look at relaxing size constraints
+
         assert H == self.img_size[0] and W == self.img_size[1], \
             f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
         
