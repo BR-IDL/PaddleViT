21530f5cd4231b56fb89557e6ec6276e36f34935
cjh3020889729 <55022974+cjh3020889729@users.noreply.github.com>
2021-11-26 13:32:38 +0800

    upload the code of mobileformer
    upload the code of mobileformer

---
+++ image_classification/MobileFormer/run_train_multi.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python main_single_gpu.py \
+    -cfg='./configs/mobileformer_26m.yaml' \
+    -dataset='imagenet2012' \
+    -num_classes=1000 \
+    -batch_size=4 \
+    -image_size=224 \
+    -data_path='/dataset/imagenet' \
+    -output='./output'
