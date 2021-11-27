154bcd2e4b48b7132675062724edaf4eed1c8533
skpig <479469418@qq.com>
2021-11-27 21:55:35 +0800

    Add MAE folder(based on ViT)

---
+++ image_classification/MAE/run_eval_multi.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=4,5,6,7 \
+python main_multi_gpu.py \
+-cfg='./configs/vit_large_patch32_384.yaml' \
+-dataset='imagenet2012' \
+-batch_size=16 \
+-data_path='/dataset/imagenet' \
+-eval \
+-pretrained='./vit_large_patch32_384.pdparams' \
---
+++ image_classification/MAE/run_train_multi.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python main_multi_gpu.py \
+-cfg='./configs/vit_base_patch16_224.yaml' \
+-dataset='imagenet2012' \
+-batch_size=8 \
+-data_path='/dataset/imagenet' \
+-amp
