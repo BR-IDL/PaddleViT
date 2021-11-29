deccdad4bfe1d51277f7297c485ed64c470dfb7e
skpig <479469418@qq.com>
2021-11-29 10:05:14 +0800

    Fix grad_clip bug

--- image_classification/MAE/run_pretrain.sh
+++ image_classification/MAE/run_pretrain.sh
@@ -2,6 +2,6 @@ CUDA_VISIBLE_DEVICES=0 \
 python main_single_gpu_pretrain.py \
 -cfg='./configs/vit_base_patch16_224_pretrain.yaml' \
 -dataset='imagenet2012' \
--batch_size=16 \
+-batch_size=32 \
 -data_path='./dataset/imagenet' \
 -amp
