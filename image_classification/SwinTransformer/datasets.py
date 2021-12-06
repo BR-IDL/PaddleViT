59dad41ad2237653bdae07a57ccc27e195c0ff33
QuanHao Guo <49911294+GuoQuanhao@users.noreply.github.com>
2021-12-06 20:02:30 +0800

    Update datasets.py
    assert mode in ['train', 'val'] to assert mode in ['train', 'test'] #81

--- image_classification/SwinTransformer/datasets.py
+++ image_classification/SwinTransformer/datasets.py
@@ -152,7 +152,7 @@ def get_dataset(config, mode='train'):
         dataset: dataset object
     """
 
-    assert mode in ['train', 'val']
+    assert mode in ['train', 'test']
     if config.DATA.DATASET == "cifar10":
         if mode == 'train':
             dataset = datasets.Cifar10(mode=mode, transform=get_train_transforms(config))
