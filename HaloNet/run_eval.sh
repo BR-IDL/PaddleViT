0cbf36694876ff1c3f1f7afeca1414b9762f4282
xperzy <xperzy@gmail.com>
2021-12-09 14:17:19 +0800

    update coding style for halonet

--- HaloNet/run_eval.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 \
-python main_single_gpu.py \
-    -cfg='./configs/halonet_50ts_256.yaml' \
-    -dataset='imagenet2012' \
-    -batch_size=16 \
-    -data_path='/dataset/imagenet' \
-    -eval \
-    -pretrained='./halonet_50ts_256'
