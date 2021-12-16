25138efdfecb001feba02742634a0df1f8c23a1a
liuhui0401 <dhwlh123@163.com>
2021-12-17 00:12:05 +0800

    improve code style

--- image_classification/BoTNet/botnet.py
+++ image_classification/BoTNet/botnet.py
@@ -1,35 +1,75 @@
+# Copyright (c) 2021 PPViT Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+Implement BoTNet
+'''
+
 import paddle
 import paddle.nn as nn
 from resnet import resnet50
 import numpy as np
 
+
 def expand_dim(t, dim, k):
+    """
+
+    Expand dims for t at dim to k
+
+    """
     t = t.unsqueeze(axis=dim)
     expand_shape = [-1] * len(t.shape)
     expand_shape[dim] = k
     return paddle.expand(t, expand_shape)
 
+
 def rel_to_abs(x):
+    """
+
+    x: [B, Nh * H, L, 2L - 1]
+    Convert relative position between the key and query to their absolute position respectively.
+    
+    """
     B, Nh, L, _ = x.shape
     # pad to shift from relative to absolute indexing
     col_pad = paddle.zeros([B, Nh, L, 1])
     x = paddle.concat([x, col_pad], axis=3)
-    flat_x = paddle.reshape(x, [B, Nh, L * 2 * L])
+    flat_x = x.reshape([B, Nh, L * 2 * L])
     flat_pad = paddle.zeros([B, Nh, L - 1])
     flat_x = paddle.concat([flat_x, flat_pad], axis=2)
     # Reshape and slice out the padded elements
-    final_x = paddle.reshape(flat_x, [B, Nh, L + 1, 2 * L - 1])
+    final_x = flat_x.reshape([B, Nh, L + 1, 2 * L - 1])
     return final_x[:, :, :L, L - 1 :]
 
+
 def relative_logits_1d(q, rel_k):
+    """
+
+    q: [B, Nh, H, W, d]
+    rel_k: [2W - 1, d]
+    Computes relative logits along one dimension.
+
+    """
     B, Nh, H, W, _ = q.shape
     rel_logits = paddle.matmul(q, rel_k.T)
     # Collapse height and heads
-    rel_logits = paddle.reshape(rel_logits, [-1, Nh * H, W, 2 * W - 1])
+    rel_logits = rel_logits.reshape([-1, Nh * H, W, 2 * W - 1])
     rel_logits = rel_to_abs(rel_logits)
-    rel_logits = paddle.reshape(rel_logits, [-1, Nh, H, W, W])
+    rel_logits = rel_logits.reshape([-1, Nh, H, W, W])
     rel_logits = expand_dim(rel_logits, dim=3, k=H)
     return rel_logits
+
 
 class RelPosEmb(nn.Layer):
     def __init__(self, 
@@ -37,38 +77,41 @@ class RelPosEmb(nn.Layer):
                 width, 
                 dim_head):
         super().__init__()
+
         scale = dim_head ** -0.5
         self.height = height
         self.width = width
-        tmp_h = paddle.randn(shape=[height * 2 - 1, dim_head]) * scale
-        tmp_w = paddle.randn(shape=[width * 2 - 1, dim_head]) * scale
+        h_shape = [height * 2 - 1, dim_head]
+        w_shape = [width * 2 - 1, dim_head]
         self.rel_height = paddle.create_parameter(
-            tmp_h.shape, dtype='float32', default_initializer=paddle.nn.initializer.Assign(tmp_h)
+            shape=h_shape, dtype='float32',
+            default_initializer=paddle.nn.initializer.Assign(paddle.randn(h_shape)*scale)
         )
         self.rel_width = paddle.create_parameter(
-            tmp_w.shape, dtype='float32', default_initializer=paddle.nn.initializer.Assign(tmp_w)
+            shape=w_shape, dtype='float32',
+            default_initializer=paddle.nn.initializer.Assign(paddle.randn(w_shape)*scale)
         )
-    
-    def forward(self, q):
-        h = self.height
-        w = self.width
-        q = paddle.reshape(q, [q.shape[0], q.shape[1], h, w, q.shape[3]]) # "b h (x y) d -> b h x y d"
-        rel_logits_w = relative_logits_1d(q, self.rel_width)
-        rel_logits_w = paddle.transpose(rel_logits_w, perm=[0, 1, 2, 4, 3, 5])
-        rel_logits_w = paddle.reshape(
-            rel_logits_w,
-            [rel_logits_w.shape[0], rel_logits_w.shape[1], rel_logits_w.shape[2]*rel_logits_w.shape[4], -1]
-        ) # "b h x i y j-> b h (x y) (i j)"
 
-        q = paddle.transpose(q, perm=[0, 1, 3, 2, 4]) # "b h x y d -> b h y x d"
+    def forward(self, q):
+
+        H = self.height
+        W = self.width
+        B, N, _, D = q.shape
+        q = q.reshape([B, N, H, W, D]) # "B N (H W) D -> B N H W D"
+        rel_logits_w = relative_logits_1d(q, self.rel_width)
+        rel_logits_w = rel_logits_w.transpose(perm=[0, 1, 2, 4, 3, 5])
+        B, N, X, I, Y, J = rel_logits_w.shape
+        rel_logits_w = rel_logits_w.reshape([B, N, X*Y, I*J]) # "B N X I Y J-> B N (X Y) (I J)"
+
+        q = q.transpose(perm=[0, 1, 3, 2, 4]) # "B N H W D -> B N W H D"
         rel_logits_h = relative_logits_1d(q, self.rel_height)
-        rel_logits_h = paddle.transpose(rel_logits_h, perm=[0, 1, 4, 2, 5, 3])
-        rel_logits_h = paddle.reshape(
-            rel_logits_h,
-            [rel_logits_h.shape[0], rel_logits_h.shape[1], rel_logits_h.shape[2]*rel_logits_h.shape[4], -1]
-        ) # "b h x i y j -> b h (y x) (j i)"
+        rel_logits_h = rel_logits_h.transpose(perm=[0, 1, 4, 2, 5, 3])
+        B, N, X, I ,Y, J = rel_logits_h.shape
+        rel_logits_h = rel_logits_h.reshape([B, N, Y*X, J*I]) # "B N X I Y J -> B N (Y X) (J I)"
+
         return rel_logits_w + rel_logits_h
-    
+
+
 class BoTBlock(nn.Layer):
     def __init__(self,
                 dim,
@@ -80,23 +123,28 @@ class BoTBlock(nn.Layer):
                 dim_qk=128,
                 dim_v=128,
                 rel_pos_emb=False,
-                activation=nn.ReLU(),):
+                activation=nn.ReLU()):
+        """
+        dim: channels in feature map
+        dim_out: output channels for feature map
+        """
         super().__init__()
+
         if dim != dim_out or stride != 1:
             self.shortcut = nn.Sequential(
                 nn.Conv2D(dim, dim_out, kernel_size=1, stride=stride, bias_attr=False),
-                nn.BatchNorm2D(dim_out, momentum=0.1),
+                nn.BatchNorm2D(dim_out),
                 activation,
             )
         else:
             self.shortcut = nn.Identity()
 
-        bottleneck_dimension = dim_out // proj_factor  # from 2048 to 512
+        bottleneck_dimension = dim_out // proj_factor
         attn_dim_out = heads * dim_v
 
         self.net = nn.Sequential(
             nn.Conv2D(dim, bottleneck_dimension, kernel_size=1, stride=1, bias_attr=False),
-            nn.BatchNorm2D(bottleneck_dimension, momentum=0.1),
+            nn.BatchNorm2D(bottleneck_dimension),
             activation,
             MHSA(
                 dim=bottleneck_dimension,
@@ -106,11 +154,11 @@ class BoTBlock(nn.Layer):
                 dim_v=dim_v,
                 rel_pos_emb=rel_pos_emb,
             ),
-            nn.AvgPool2D(2) if stride == 2 else nn.Identity(),  # same padding
-            nn.BatchNorm2D(attn_dim_out, momentum=0.1),
+            nn.AvgPool2D(2) if stride == 2 else nn.Identity(),
+            nn.BatchNorm2D(attn_dim_out),
             activation,
             nn.Conv2D(attn_dim_out, dim_out, kernel_size=1, stride=1, bias_attr=False),
-            nn.BatchNorm2D(dim_out, momentum=0.1),
+            nn.BatchNorm2D(dim_out),
         )
 
         self.activation = activation
@@ -121,7 +169,9 @@ class BoTBlock(nn.Layer):
         featuremap += shortcut
         return self.activation(featuremap)
 
+
 class MHSA(nn.Layer):
+    '''Multi-Head Self-Attention'''
     def __init__(self, 
                 dim, 
                 fmap_size, 
@@ -129,7 +179,14 @@ class MHSA(nn.Layer):
                 dim_qk=128, 
                 dim_v=128, 
                 rel_pos_emb=False):
+        """
+        dim: number of channels of feature map
+        fmap_size: [H, W]
+        dim_qk: vector dimension for q, k
+        dim_v: vector dimension for v (not necessarily the same with q, k)
+        """
         super().__init__()
+
         self.scale = dim_qk ** -0.5
         self.heads = heads
         out_channels_qk = heads * dim_qk
@@ -142,35 +199,35 @@ class MHSA(nn.Layer):
         height, width = fmap_size
         self.pos_emb = RelPosEmb(height, width, dim_qk)
 
+    def transpose_multihead(self, x):
+        B, N, H, W = x.shape
+        x = x.reshape([B, self.heads, -1, H, W]) # "B (h D) H W -> B h D H W"
+        x = x.transpose(perm=[0, 1, 3, 4, 2])    # "B h D H W -> B h H W D"
+        x = x.reshape([B, self.heads, H*W, -1])  # "B h H W D -> B h (H W) D"
+        return x
+
     def forward(self, featuremap):
-        heads = self.heads
+        """
+        featuremap: [B, d_in, H, W]
+        Output: [B, H, W, head * d_v]
+        """
         B, C, H, W = featuremap.shape
         q, k = self.to_qk(featuremap).chunk(2, axis=1)
         v = self.to_v(featuremap)
-        
-        q = paddle.reshape(q, [q.shape[0], heads, -1, q.shape[2], q.shape[3]])
-        q = paddle.transpose(q, perm=[0, 1, 3, 4, 2])
-        q = paddle.reshape(q, [q.shape[0], q.shape[1], -1, q.shape[4]]) # "B (h d) H W -> B h (H W) d"
-        k = paddle.reshape(k, [k.shape[0], heads, -1, k.shape[2], k.shape[3]])
-        k = paddle.transpose(k, perm=[0, 1, 3, 4, 2])
-        k = paddle.reshape(k, [k.shape[0], k.shape[1], -1, k.shape[4]]) # "B (h d) H W -> B h (H W) d"
-        v = paddle.reshape(v, [v.shape[0], heads, -1, v.shape[2], v.shape[3]])
-        v = paddle.transpose(v, perm=[0, 1, 3, 4, 2])
-        v = paddle.reshape(v, [v.shape[0], v.shape[1], -1, v.shape[4]]) # "B (h d) H W -> B h (H W) d"
-        
+        q, k, v = map(self.transpose_multihead, [q, k, v])
         q *= self.scale
 
-        logits = paddle.matmul(q, paddle.transpose(k, perm=[0, 1, 3, 2]))
+        logits = paddle.matmul(q, k.transpose(perm=[0, 1, 3, 2]))
         logits += self.pos_emb(q)
-        
+
         weights = self.softmax(logits)
         attn_out = paddle.matmul(weights, v)
-        attn_out = paddle.reshape(attn_out, [attn_out.shape[0], attn_out.shape[1], H, -1, attn_out.shape[3]])
-        attn_out = paddle.transpose(attn_out, perm=[0, 1, 4, 2, 3])
-        attn_out = paddle.reshape(
-            attn_out, [attn_out.shape[0], -1, attn_out.shape[3], attn_out.shape[4]]
-        ) # "B h (H W) d -> B (h d) H W"
+        a_B, a_N, a_, a_D = attn_out.shape
+        attn_out = attn_out.reshape([a_B, a_N, H, -1, a_D])  # "B N (H W) D -> B N H W D"
+        attn_out = attn_out.transpose(perm=[0, 1, 4, 2, 3])  # "B N H W D -> B N D H W"
+        attn_out = attn_out.reshape([a_B, a_N*a_D, H, -1]) # "B N D H W -> B (N D) H W"
         return attn_out
+
 
 class BoTStack(nn.Layer):
     def __init__(self,
@@ -185,6 +242,10 @@ class BoTStack(nn.Layer):
                 dim_v=128,
                 rel_pos_emb=False,
                 activation=nn.ReLU(),):
+        """
+        dim: channels in feature map
+        fmap_size: [H, W]
+        """
         super().__init__()
 
         self.dim = dim
@@ -222,12 +283,16 @@ class BoTStack(nn.Layer):
         assert h == self.fmap_size[0] and w == self.fmap_size[1]
         return self.net(x)
 
+
 def botnet50(pretrained=False, 
             image_size=224, 
             fmap_size=(14, 14), 
             num_classes=1000, 
             embed_dim=2048,
             **kwargs):
+    """
+    Bottleneck Transformers for Visual Recognition.
+    """
     resnet = resnet50(pretrained=False, **kwargs)
     layer = BoTStack(dim=1024, dim_out=embed_dim, fmap_size=fmap_size, stride=1, rel_pos_emb=True)
     backbone = list(resnet.children())
@@ -242,6 +307,7 @@ def botnet50(pretrained=False,
         state_dict = paddle.load('botnet50.pdparams')
         model.set_state_dict(state_dict)
     return model
+
 
 def build_botnet50(config):
     model = botnet50(
