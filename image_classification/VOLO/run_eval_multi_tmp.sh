182b5b23d95553e9629da116df5ce97ef92ff7a0
xperzy <xperzy@gmail.com>
2022-04-11 15:13:55 +0800

    refactor VOLO

--- image_classification/VOLO/run_eval_multi_tmp.sh
+++
@@ -1,9 +0,0 @@
-#CUDA_VISIBLE_DEVICES=4,5,6,7 \
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python main_multi_gpu.py \
--cfg='./configs/volo_d1_224.yaml' \
--dataset='imagenet2012' \
--batch_size=64 \
--data_path='/dataset/imagenet' \
--eval \
--pretrained='./d1_224_84.2' \
